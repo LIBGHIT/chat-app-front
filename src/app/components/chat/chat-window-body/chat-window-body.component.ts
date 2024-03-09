3f73c22a7af38faade5e61ed7ad52fed397daced
ABDESSAMAD HAIDA <115994964+LIBGHIT@users.noreply.github.com>
2024-03-09 20:12:48 +0100

    chat window fixed

--- src/app/components/chat/chat-window-body/chat-window-body.component.ts
+++ src/app/components/chat/chat-window-body/chat-window-body.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { MeMessageComponent } from '../messages/me-message/me-message.component';
 import { TheyMessageComponent } from '../messages/they-message/they-message.component';
 
 @Component({
   selector: 'app-chat-window-body',
   standalone: true,
-  imports: [MeMessageComponent,
-  TheyMessageComponent],
+  imports: [MeMessageComponent, TheyMessageComponent],
   templateUrl: './chat-window-body.component.html',
-  styleUrl: './chat-window-body.component.scss'
+  styleUrls: ['./chat-window-body.component.scss']
 })
-export class ChatWindowBodyComponent {
+export class ChatWindowBodyComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.startScrollFromBottom();
+  }
+
+  startScrollFromBottom(): void {
+    const chat_body = document.getElementById('chat-body') as HTMLDivElement;
+    if(chat_body) {
+      chat_body.scrollTop = chat_body.scrollHeight;
+    }
+  }
 }
